d66bb372350eec971a10296db74867a341bf0122
Lowder <me@lowderplay.dev>
2024-07-29 21:10:36 +0500

    fix: check if url is invalid

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,30 +1,37 @@
 import {useEffect, useState} from "react";
-import {HOTP, URI} from "otpauth";
+import {HOTP, TOTP, URI} from "otpauth";
 
 export default function useAccount(accountUri?: string): { code: string, period: number, progress: number } {
-    const [code, setCode] = useState("N/A");
-    const [period, setPeriod] = useState(30);
-    useEffect(() => {
-        if (!accountUri) return;
-        const otp = URI.parse(accountUri);
-        if (otp instanceof HOTP) {
-            throw new Error("HOTP is not supported");
-        }
+  const [code, setCode] = useState("N/A");
+  const [period, setPeriod] = useState(30);
+  useEffect(() => {
+    if (!accountUri) return;
+    let otp: HOTP | TOTP;
+    try {
+      otp = URI.parse(accountUri);
+    } catch (e) {
+      console.error("weird uri!", accountUri);
+      setCode("N/A");
+      return;
+    }
+    if (otp instanceof HOTP) {
+        throw new Error("HOTP is not supported");
+    }
 
-        setPeriod(otp.period);
-        let timeout: NodeJS.Timeout | null = null;
+    setPeriod(otp.period);
+    let timeout: NodeJS.Timeout | null = null;
 
-        function cycle() {
-            setCode(otp.generate());
-            const untilNext = period - (Math.floor(Date.now() / 1000) % period);
-            timeout = setTimeout(cycle, untilNext * 1000);
-        }
-        cycle();
+    function cycle() {
+        setCode(otp.generate());
+        const untilNext = period - (Math.floor(Date.now() / 1000) % period);
+        timeout = setTimeout(cycle, untilNext * 1000);
+    }
+    cycle();
 
-        return () => {
-            if (timeout) clearTimeout(timeout);
-        }
-    }, [accountUri, period]);
+    return () => {
+        if (timeout) clearTimeout(timeout);
+    }
+  }, [accountUri, period]);
 
 
     const [progress, setProgress] = useState(0);
