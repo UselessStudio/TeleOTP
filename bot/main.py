dda22024626cecb01bacf9ec232dabeadf7e1650
Lowder <me@lowderplay.dev>
2024-08-01 16:34:23 +0500

    feat: bot localization

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import yaml
 from telegram import Update, WebAppInfo, InlineKeyboardMarkup, \
     InlineKeyboardButton
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
@@ -13,20 +14,35 @@ logging.basicConfig(
 app_tg = os.environ['TG_APP']
 app_url = os.environ['WEBAPP_URL']
 
+translations = {}
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    lang = update.effective_user.language_code
+
+    def l(code):
+        if lang in translations and code in translations[lang]:
+            return translations[lang][code]
+
+        return translations["en"][code]
+
     keyboard = InlineKeyboardMarkup.from_button(InlineKeyboardButton(
-        text="Open TeleOTP",
+        text=l("ButtonText"),
         web_app=WebAppInfo(url=f"{app_url}/")))
 
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="👋 Welcome to TeleOTP!\n"
-                                        "I can help you protect your accounts with 2FA.\n"
-                                        "Click the button below to get started!",
+                                   text=f"{l('Welcome')}\n"
+                                        f"{l('ICanHelp')}\n"
+                                        f"{l('ClickButton')}",
                                    reply_markup=keyboard)
 
 
 if __name__ == '__main__':
+    for lang in os.listdir("lang"):
+        code = lang.replace(".yml", "")
+        with open(os.path.join("lang", lang), encoding="utf-8") as f:
+            translations[code] = yaml.safe_load(f)
+
     application = ApplicationBuilder().token(os.environ['TOKEN']).build()
 
     start_handler = MessageHandler(filters=filters.ALL, callback=start)
